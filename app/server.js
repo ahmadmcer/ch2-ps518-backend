c994a33580ab16efdcf0f900a60a08e4b1e1d944
ahmadmcer <ahmadmcer@gmail.com>
2023-12-04 16:26:33 +0700

    fix: recode for app engine deploy

--- app/server.js
+++ app/server.js
@@ -1,37 +1,16 @@
-// server.js
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
-    port: 3000,
-    host: 'localhost',
+    port: process.env.PORT || 8080,
+    host: '0.0.0.0',
   });
 
   server.route(routes);
 
-  // Tambahkan handling untuk kesalahan server
-  server.ext('onPreResponse', (request, h) => {
-    const { response } = request;
-
-    if (response.isBoom) {
-      console.error(response);
-      return h.response({
-        statusCode: 500,
-        error: 'Internal Server Error',
-        message: 'An internal server error occurred',
-      }).code(500);
-    }
-
-    return h.continue;
-  });
-
-  try {
-    await server.start();
-    console.log(`Server running on ${server.info.uri}`);
-  } catch (error) {
-    console.error('Error starting server:', error.message);
-  }
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
 };
 
 process.on('unhandledRejection', (err) => {
